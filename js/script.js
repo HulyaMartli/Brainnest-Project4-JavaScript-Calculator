edbc3ad6eef445d6ff926601109acc72dfe705fb
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-05-12 23:07:27 +0300

    formatting edits

--- js/script.js
+++ js/script.js
@@ -169,8 +169,8 @@ span.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target == modal){
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
 }
